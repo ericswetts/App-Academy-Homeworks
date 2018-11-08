c035d1ae26102f4766d90057675d4aed2ff2d4b8
ericswetts <eric.swetts@gmail.com>
2018-11-08 08:58:09 -0800

    W3D3 homework- active_record_warmup

--- W3D2/Intro_Rails/db/migrate/20181107164132_houses.rb
+++ W3D2/Intro_Rails/db/migrate/20181107164132_houses.rb
@@ -1,7 +1,7 @@
 class Houses < ActiveRecord::Migration[5.2]
   def change
     create_table :houses do |p|
-      p.string :address, null: false
+      p.string :address, null: false, unique: true
       p.timestamps
     end
   end
